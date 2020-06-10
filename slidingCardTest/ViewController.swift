0f16b0d8e8a0d5db3420dd78130f771e50bc1507
shinohara-san <someiyoshino0809@gmail.com>
2020-06-10 13:38:50 +0900

    first commmit

--- slidingCardTest/ViewController.swift
+++ slidingCardTest/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import CardSlider
 
-class ViewController: UIViewController {
+struct Item: CardSliderItem{
+    var image: UIImage
+    var rating: Int?
+    var title: String
+    var subtitle: String?
+    var description: String?
+}
 
+class ViewController: UIViewController, CardSliderDataSource {
+    
+    @IBOutlet var myButton: UIButton!
+    
+    var data = [Item]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        data.append(Item(image: UIImage(named: "hinako")!,
+                         rating: nil,
+                         title: "BookMarks",
+                         subtitle: "Get exceited about them",
+                         description: "You can add the bookmarks now"))
+        data.append(Item(image: UIImage(named: "kasumi")!,
+        rating: nil,
+        title: "BookMarks",
+        subtitle: "Get exceited about them",
+        description: "You can add the bookmarks now"))
+        data.append(Item(image: UIImage(named: "mikako")!,
+        rating: nil,
+        title: "BookMarks",
+        subtitle: "Get exceited about them",
+        description: "You can add the bookmarks now"))
     }
 
-
+    @IBAction func didTapButton(_ sender: Any) {
+        let vc = CardSliderViewController.with(dataSource: self)
+        vc.title = "welcome"
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
+    func item(for index: Int) -> CardSliderItem {
+        data[index]
+    }
+    
+    func numberOfItems() -> Int {
+        data.count
+    }
+    
 }
 
